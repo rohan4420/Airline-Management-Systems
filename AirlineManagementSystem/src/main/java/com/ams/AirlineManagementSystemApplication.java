2e52c8afa83f6b136aeb6bf5081a9b83fb0f965f
Aditya <adityagundg14@gmail.com>
2024-02-28 11:31:08 +0530

    Initial commit

---
+++ AirlineManagementSystem/src/main/java/com/ams/AirlineManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineManagementSystemApplication.class, args);
+		System.out.println("Spring Boot is started");
+	}
+
+}
